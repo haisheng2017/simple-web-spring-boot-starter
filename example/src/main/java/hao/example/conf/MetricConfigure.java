46cc8c73e0c18c068608346e60075186f2878882
haisheng2017 <895883668@qq.com>
2023-09-05 22:54:08 +0800

    prometheus metric

---
+++ example/src/main/java/hao/example/conf/MetricConfigure.java
@@ -0,0 +1,17 @@
+package hao.example.conf;
+
+import io.micrometer.core.aop.TimedAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by hào on 2023/9/5
+ */
+@Configuration
+public class MetricConfigure {
+
+    @Bean
+    public TimedAspect timedAspect() {
+        return new TimedAspect();
+    }
+}
--- example/src/main/java/hao/example/controller/UserController.java
+++ example/src/main/java/hao/example/controller/UserController.java
@@ -3,6 +3,7 @@ package hao.example.controller;
 import hao.example.mapper.UserMapper;
 import hao.example.model.User;
 import hao.simple.exception.SimpleThrower;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
+    @Timed
     @GetMapping
     public List<User> list() {
         return userMapper.selectList(null);
